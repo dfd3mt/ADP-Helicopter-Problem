a1ceed05ac20f1c69a922f4eb888e950e6b5f9c6
pjgrymes <james.grymes@usma.edu>
2016-09-01 16:19:51 -0400

    Update Supply.m

--- Supply.m
+++ Supply.m
@@ -1,11 +1,13 @@
+%make this a function that takes in params and outputs variables
+
 AvailBH=4;                          %Number of Black Hawk pairs available
 BHAttrib=3;                         %Number of required black hawk attributes
 AvailTeams=5;                       %Number of available Flight crews each for a team of two black hawks to start day
-MaxFuel=360*ones(AvailBH,1);        %Total amount of gallons of fuel a BH can hold
-CapRem=4000*rand(AvailBH,1);   %Weight Capacity Remaining in lbs
+MaxFuel=360; %ones(AvailBH,1);        %Total amount of gallons of fuel a BH can hold
+CapRem=4000;    %4000*rand(AvailBH,1);   %Weight Capacity Remaining in lbs
 %TTMinorMaint=15;                   %Amount of time until next scheduled minor maintenence
 %TTSigMaint=50;                     %Amount of time until next scheduled significant maintenence
-InFlight=randsample([0,1],AvailBH,true);    %Indicator for whether or not the bird is inflight
+InFlight=0;  %randsample([0,1],AvailBH,true);    %Indicator for whether or not the bird is inflight
 BlackHawk=zeros(AvailBH,BHAttrib);  %Preallocate matrix
 BlackHawk(:,1)=MaxFuel;             
 BlackHawk(:,2)=CapRem;
